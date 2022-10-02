37a31f8311333ba3ca5407321bfe64d51c668944
fishy <omg@lol.xxx>
2022-10-02 09:02:11 +0200

    Update the title

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Broadway Demo Site</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
